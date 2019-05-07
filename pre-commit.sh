0a858fb320f82f6ff0cd6417915116d05c8eb5bc
Milkey Mouse <milkeymouse@meme.institute>
2019-05-06 21:52:18 -0700

    Test Jekyll build in pre-commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-
 git diff --name-only --cached assets | xargs -r exiftool -all= -overwrite_original
 git diff --name-only --cached assets | grep '.pdf$' | xargs -r qpdf --linearize
 git diff --name-only --cached assets | xargs -r git add
 
-exit
+# don't commit if build fails
+# TODO: this checks the working dir version, not the staged version
+TMPDIR="$(mktemp -p /tmp -d jekyll-XXXXX)"
+bundle exec jekyll build "${TMPDIR}"
+CODE=$?
+
+rm -r "${TMPDIR}"
+
+exit ${CODE}
