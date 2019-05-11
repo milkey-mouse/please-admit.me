4f3606faaf8938924042feccf85394704b4c88a4
Milkey Mouse <milkeymouse@meme.institute>
2019-05-10 22:28:53 -0700

    Add header links with AnchorJS

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-git diff --name-only --cached assets | xargs -r exiftool -all= -overwrite_original
-git diff --name-only --cached assets | grep '.pdf$' | xargs -r qpdf --linearize
-git diff --name-only --cached assets | xargs -r git add
+
+git diff --name-only --cached -z | grep -z '\.md$' | xargs -0rn1 \
+sh -c 'sed -i "/^anchors: true$/d" "$1"; sed "2,/^---$/ d" "$1" | grep -q "^#" && sed -i "0,/^---$/! {0,/^---$/ s/^---$/anchors: true\n---/}" "$1"; git add "$1"' sh
+
+# remove EXIF and other metadata from photos & other media (sometimes this even includes geotags!)
+git diff --name-only --cached -z assets | xargs -0rn1 sh -c 'exiftool -all= -overwrite_original "$1" && git add "$1"' sh
+
+# PDFs must be linearized or deletion is reversible
+git diff --name-only --cached -z assets | grep -z '\.pdf$' | xargs -0rn1 sh -c 'qpdf --linearize "$1" && git add "$1"' sh
 
 # don't commit if build fails
 # TODO: this checks the working dir version, not the staged version
